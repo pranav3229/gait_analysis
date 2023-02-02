efa5f69653bcdb048a3ce0c93056c920e93d2aff
Shreyas Sesham <55966833+shreyas1209@users.noreply.github.com>
2023-02-02 21:15:12 +0530

    Temp model  class to store new patient data

---
+++ GAIT_Analysis/models/patients.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Patient{
+  String patientName;
+  String patientID;
+  int patientPhoneNumber;
+  String patientAddress;
+  String patientDOB;
+  String patientGender;
+  int patientHeight;
+  int patientWeight;
+  Patient({required this.patientName,required this.patientID,required this.patientPhoneNumber,required this.patientAddress,required this.patientDOB,required this.patientGender,required this.patientHeight,required this.patientWeight});
+}
