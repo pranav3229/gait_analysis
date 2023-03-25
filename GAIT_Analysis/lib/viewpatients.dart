ac11119bbdfbee4aab9237ebb1d94e0d6c74b754
pranav3229 <f20212685@pilani.bits-pilani.ac.in>
2023-03-26 00:59:31 +0530

    Rewriting of listview builder in efficient way

--- GAIT_Analysis/lib/viewpatients.dart
+++ GAIT_Analysis/lib/viewpatients.dart
@@ -5,6 +5,7 @@ import 'package:gait_analysis/patientprofile.dart';
 
 import 'homescreen.dart';
 
+
 class patientviewstless extends StatelessWidget {
   const patientviewstless({Key? key}) : super(key: key);
 
@@ -32,82 +33,201 @@ class _patientviewState extends State<patientview> {
   late String text_phone;
   late String text_profurl;
   late String text_weight;
+  bool _searchBoolean = false;
+  Widget _searchTextField() {
+    return TextField(
+      autofocus: true, //Display the keyboard when TextField is displayed
+      cursorColor: Colors.white,
+      style: TextStyle(
+        color: Colors.white,
+        fontSize: 20,
+      ),
+      textInputAction: TextInputAction.search, //Specify the action button on the keyboard
+      decoration: InputDecoration( //Style of TextField
+        enabledBorder: UnderlineInputBorder( //Default TextField border
+            borderSide: BorderSide(color: Colors.white)
+        ),
+        focusedBorder: UnderlineInputBorder( //Borders when a TextField is in focus
+            borderSide: BorderSide(color: Colors.white)
+        ),
+        hintText: 'Search', //Text that is displayed when nothing is entered.
+        hintStyle: TextStyle( //Style of hintText
+          color: Colors.white60,
+          fontSize: 20,
+        ),
+      ),
+    );
+  }
+  Widget _defaultListView(){
+    return StreamBuilder(
+      stream: FirebaseFirestore.instance.collection('patients').snapshots(),
+      builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (!snapshot.hasData) {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+
+        return ListView(
+          shrinkWrap: true,
+          children: snapshot.data!.docs.map((document) {
+            return Container(
+                height: 125,
+                child: Ink(
+
+                  child: InkWell(
+                    splashColor: Colors.tealAccent,
+                    onTap: () {
+                      print(document.id);
+                      print(document['name']);
+                      print(document['dob']);
+                      print(document['gender']);
+                      print(document['height']);
+                      print(document['phone']);
+                      print(document['profile picture URL']);
+                      print(document['weight']);
+                      text_id = document.id;
+                      text_name = document['name'];
+                      text_dob = document['dob'];
+                      text_gender = document['gender'];
+                      text_height = document['height'];
+                      text_phone = document['phone'];
+                      text_profurl = document['profile picture URL'];
+                      text_weight = document['weight'];
+                      Navigator.of(context).pushReplacement(MaterialPageRoute(
+                          builder: (context) => patientprofile(
+                              text_name,
+                              text_id,
+                              text_dob,
+                              text_gender,
+                              text_height,
+                              text_phone,
+                              text_profurl,
+                              text_weight)));
+                    },
+                    child: new Card(
+
+                        child: Center(
+                            child: Column(
+                              children: [
+                                SizedBox(height: 25),
+                                Text(document['name']),
+                                SizedBox(height: 35),
+                                Text('ID: ${document.id}'),
+                              ],
+                            ))),
+                  ),
+                ));
+          }).toList(),
+        );
+      },
+    );
+  }
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        actions: !_searchBoolean
+            ? [
+          IconButton(
+            // color: Colors.black,
+              icon: Icon(Icons.search),
+              onPressed: () {
+                setState(() {
+                  _searchBoolean = true;
+                });
+              })
+        ]
+            : [
+          IconButton(
+
+              icon: Icon(Icons.clear),
+              onPressed: () {
+                setState(() {
+                  _searchBoolean = false;
+                });
+              }
+          )
+        ],
         leading: BackButton(
             onPressed: () {
               Navigator.of(context).pushReplacement(
                   MaterialPageRoute(builder: (context) => homescreen()));
             },
-            color: Colors.black),
+            color: Colors.white),
+
         centerTitle: true,
         backgroundColor: Colors.green,
-        title: Text('View Patients'),
+        title: !_searchBoolean ? Text('View Patients') : _searchTextField(),
       ),
-      body: StreamBuilder(
-        stream: FirebaseFirestore.instance.collection('patients').snapshots(),
-        builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
-          if (!snapshot.hasData) {
-            return Center(
-              child: CircularProgressIndicator(),
-            );
-          }
+      body: Container(
+        child: _defaultListView(),
+        // child: StreamBuilder(
+        //   stream: FirebaseFirestore.instance.collection('patients').snapshots(),
+        //   builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        //     if (!snapshot.hasData) {
+        //       return Center(
+        //         child: CircularProgressIndicator(),
+        //       );
+        //     }
+        //
+        //     return ListView(
+        //       shrinkWrap: true,
+        //       children: snapshot.data!.docs.map((document) {
+        //         return Container(
+        //             height: 125,
+        //             child: Ink(
+        //
+        //               child: InkWell(
+        //                 splashColor: Colors.tealAccent,
+        //                 onTap: () {
+        //                   print(document.id);
+        //                   print(document['name']);
+        //                   print(document['dob']);
+        //                   print(document['gender']);
+        //                   print(document['height']);
+        //                   print(document['phone']);
+        //                   print(document['profile picture URL']);
+        //                   print(document['weight']);
+        //                   text_id = document.id;
+        //                   text_name = document['name'];
+        //                   text_dob = document['dob'];
+        //                   text_gender = document['gender'];
+        //                   text_height = document['height'];
+        //                   text_phone = document['phone'];
+        //                   text_profurl = document['profile picture URL'];
+        //                   text_weight = document['weight'];
+        //                   Navigator.of(context).pushReplacement(MaterialPageRoute(
+        //                       builder: (context) => patientprofile(
+        //                           text_name,
+        //                           text_id,
+        //                           text_dob,
+        //                           text_gender,
+        //                           text_height,
+        //                           text_phone,
+        //                           text_profurl,
+        //                           text_weight)));
+        //                 },
+        //                 child: new Card(
+        //
+        //                     child: Center(
+        //                         child: Column(
+        //                   children: [
+        //                     SizedBox(height: 25),
+        //                     Text(document['name']),
+        //                     SizedBox(height: 35),
+        //                     Text('ID: ${document.id}'),
+        //                   ],
+        //                 ))),
+        //               ),
+        //             ));
+        //       }).toList(),
+        //     );
+        //   },
+        // ),
 
-          return GlowingOverscrollIndicator(
-            // behavior: ScrollBehavior(),
-            axisDirection: AxisDirection.down,
-            color: Colors.yellowAccent,
-            child: ListView(
-              children: snapshot.data!.docs.map((document) {
-                return Container(
-                    height: 125,
-                    child: InkWell(
-                      onTap: () {
-                        print(document.id);
-                        print(document['name']);
-                        print(document['dob']);
-                        print(document['gender']);
-                        print(document['height']);
-                        print(document['phone']);
-                        print(document['profile picture URL']);
-                        print(document['weight']);
-                        text_id = document.id;
-                        text_name = document['name'];
-                        text_dob = document['dob'];
-                        text_gender = document['gender'];
-                        text_height = document['height'];
-                        text_phone = document['phone'];
-                        text_profurl = document['profile picture URL'];
-                        text_weight = document['weight'];
-                        Navigator.of(context).pushReplacement(MaterialPageRoute(
-                            builder: (context) => patientprofile(
-                                text_name,
-                                text_id,
-                                text_dob,
-                                text_gender,
-                                text_height,
-                                text_phone,
-                                text_profurl,
-                                text_weight)));
-                      },
-                      child: new Card(
-                          child: Center(
-                              child: Column(
-                        children: [
-                          SizedBox(height: 25),
-                          Text(document['name']),
-                          SizedBox(height: 35),
-                          Text('ID: ${document.id}'),
-                        ],
-                      ))),
-                    ));
-              }).toList(),
-            ),
-          );
-        },
       ),
     );
   }
